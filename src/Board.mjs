83d4e07d98f8ec76517c4c18df53b88b3f29479b
000hcl <mnrjohansson@gmail.com>
2023-02-14 18:18:00 +0200

    has falling test passes

--- src/Board.mjs
+++ src/Board.mjs
@@ -6,6 +6,7 @@ export class Board {
     this.width = width;
     this.height = height;
     this.currentBoard = `...\n...\n...\n`
+    this.currentBlock = -3
   }
 
   toString() {
@@ -18,14 +19,25 @@ export class Board {
     if (this.currentBoard !== `...\n...\n...\n`) {
       throw "already falling"
     }
+    this.currentBlock += 4
     var subStringA = this.currentBoard.substring(0,1)
     var subStringB = this.currentBoard.substring(2)
     this.currentBoard = subStringA + 'X' + subStringB
   }
 
   tick () {
-    var subStringA = this.currentBoard.substring(3,5)
-    var subStringB = this.currentBoard.substring(6)
-    this.currentBoard = '...' + subStringA + 'X' + subStringB
+    /*empty until current block location*/
+    var subStringA = this.currentBoard.substring(0,this.currentBlock)
+    var subStringB = this.currentBoard.substring(this.currentBlock+1)
+    this.currentBoard = subStringA + '.' + subStringB
+    /*add new falling block*/
+    this.currentBlock += 4
+    var subStringA = this.currentBoard.substring(0,this.currentBlock)
+    var subStringB = this.currentBoard.substring(this.currentBlock+1)
+    this.currentBoard = subStringA + 'X' + subStringB
+  }
+
+  hasFalling () {
+    return true;
   }
 }
